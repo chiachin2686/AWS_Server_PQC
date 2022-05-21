2b401d8174273ef8563a7513b207322d43db49df
chiachin2686 <chiachin112686@gmail.com>
2022-05-22 01:00:07 +0800

    Update check_file.sh

--- RESTful_API (send data)/check_file.sh
+++ RESTful_API (send data)/check_file.sh
@@ -3,20 +3,28 @@
 function file_checked(){
     local file_path="/db/data/logfile.txt"
     local check_time=1
+    counter=0  # set a counter
     while [[ true ]]; do
         file_old_stat="`stat ${file_path}|grep Modify`"
         sleep ${check_time}
         file_new_stat="`stat ${file_path}|grep Modify`"
         if [ -f ${file_path} ]; then  # file exists
             if [[ `echo ${file_old_stat}` == `echo ${file_new_stat}` ]]; then
-                echo "### In ${check_time}s, ${file_path} doesn't change ###"
+                echo "### File doesn't change ###"
             else
                 grep "time" ${file_path} | tail -1 > /db/data/mqtt_sensordata.txt  # get the last data
-                echo "### In ${check_time}s, ${file_path} changes ###"
+                if [[ "$counter" -gt 1000 ]]; then  # if counter > 1000, reset the log file
+                    rm ${file_path}
+                    touch ${file_path}
+                    counter=0
+                else
+                    counter=$((counter+1))
+                fi
+                echo "### File changes ###"
             fi
-            echo "${file_path} exists."
+            echo "File exists."
         else
-            echo "${file_path} does not exists."
+            echo "File does not exists."
         fi
     done
 }
